ecc430b70e2e8bc56fd4e1443824a1f3941f2a3c
K.Harshitha <23eg105e25@anurag.edu.in>
2025-10-17 06:15:59 +0000

    Change the mark beside ClinicOps to Hospital symbol

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -3,18 +3,21 @@ import type { SVGProps } from 'react';
 export function Logo(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
     >
-        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5" />
-        <path d="M12 8v8" />
-      <path d="M8 12h8" />
+      <path d="M12 6v12" />
+      <path d="M18 6v12" />
+      <path d="M6 6v12" />
+      <path d="M12 18H6a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2h-6" />
+      <path d="M10 12h4" />
+      <path d="M12 10v4" />
     </svg>
   );
 }
